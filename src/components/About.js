d558a0356e9a065c3773026cc876499aeef7fec2
MichaelLorenzo05 <michael.lorenzo.2002@gmail.com>
2024-09-13 20:58:45 -0700

    about page info

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,17 @@ function About() {
         <div className="about">
             <h1>My Story</h1>
             <h2>Finding Inspiration in Every Turn</h2>
-            <p>This is your About Page. This space is a great opportunity to give a full background on who you are, what you do, and what your website has to offer. Double click on the text box to start editing your content and make sure to add all the relevant details you want site visitors to know.</p>
+            <p>Through my fifties, I considered myself to be a healthy, fit individual. I didn’t have any chronic conditions (though parents and grands did). I had no allergies, I exercised regularly and I slept well. With the exception of semi-annual PCP and OB/GYN visits, I rarely found myself in need of a doctor. In my early 50s I was diagnosed with Osteopenia and 5 years later with Osteoporosis. When I began peri-menopause, my GYN asked if I wanted HRT, but since my mom had been through a bout of post-menopause breast cancer, I opted out.
+            Through self study and working with a Functional Medicine doctor, I have come to learn the importance of bio-identical HRT. I have become focused on improving my bone density with a combination of yoga, HIIT and weight lifting. I have also improved my incontinence with a regular pelvic floor strengthening routine. I have learned the importance of keeping my hormones inn balance and have started HRT.</p>
+            <h2>My Goals</h2>
+            <div className="aboutList">
+            <ul>
+            <li>I want to save other women the uncertainty of deciding to opt in or out of HRT.</li>
+            <li>I want to help them stay strong and upright for as long as possible.</li>
+            <li>I want to help them improve their sleep, reduce their stress and make the lifestyle changes they want to do achieve these goals.</li>
+            <li>I want to help them balance their hormones and find balance in their lives,</li>
+            </ul>
+            </div>
         </div>
     );
 }
