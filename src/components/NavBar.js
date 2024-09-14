be9ada5f035a6625f1040651b27fd5120ec4fac1
cxplanet <cxplanet@hotmail.com>
2024-09-14 14:10:04 -0700

    Trimmed border of svg, and resized it in code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
-import { ReactComponent as Logo } from '../img/kml-trans-white.svg';
+import { ReactComponent as Logo } from '../img/kmlhome.svg';
 
 function Navbar() {
     return (
         <nav className="navbar">
             <div className="navbar-content">
                 <div className="navbar-inner">
-                    <Logo className="logo" style={{ width: '200px', height: 'auto' }} />
+                    <Logo className="logo" style={{ width: '160px', height: 'auto' }} />
                     <ul className="nav-links">
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/about">About</Link></li>
