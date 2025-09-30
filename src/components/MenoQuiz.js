a9a12ec5628908c67315d4ed8d28c7bb808c58e3
cxplanet <cxplanet@hotmail.com>
2025-09-30 13:51:23 -0700

    Update MenoQuiz.js
    added debugging

--- src/components/MenoQuiz.js
+++ src/components/MenoQuiz.js
@@ -157,9 +157,9 @@ export default function MenoQuiz() {
     let score = 0;
     
     // Check if any answers have been selected
-    alert(answers);
+    window.alert(answers);
     if (Object.keys(answers).length === 0) {
-      alert('Please answer at least one question before submitting.');
+      window.alert('Please answer at least one question before submitting.');
       return;
     }
 
@@ -226,6 +226,7 @@ export default function MenoQuiz() {
         <button type="button" onClick={calculateScore}>Submit</button>
       </form>
 
+window.alert('Hi');
       {result && (
         <div 
           ref={resultRef}
