81162529691fbe57619b696bdf5a44f7418ece1f
cxplanet <cxplanet@hotmail.com>
2025-12-02 15:23:05 -0800

    Update About.js
    </li> fix

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,12 @@ function About() {
             <p>Through my forties and early fifties I considered myself to be a relatively healthy, fit individual. I exercised, ran and practiced yoga regularEnter ly and I slept well. With the exception of semi-annual PCP and GYN visits, I rarely found myself in need of a doctor.</p>
             <p>Enter Perimenopause!</p>
             <p><ul>
-            <li>An osteopenia diagnosis, followed in short order by osteoporosis made me feel old and frail despite my active lifestyle.
-            <li>Hot flashes that felt like I was roasting from the inside out.
-            <li>Night sweats that left me fumbling in the dark to change my PJs and find a dry place to sleep without waking my husband.
-            <li>Raging arguments with family members that I couldn’t stop even though a voice inside me was telling me I was being irrational made me question my sanity.
-            <li>Unexplained weight gain and belly fat that seemed to happen overnight.
-            <li>Rushing to find a bathroom or planning my trips to include frequent bathroom stops for fear of an accident.</ul>
+            <li>An osteopenia diagnosis, followed in short order by osteoporosis made me feel old and frail despite my active lifestyle.</li>
+            <li>Hot flashes that felt like I was roasting from the inside out.</li>
+            <li>Night sweats that left me fumbling in the dark to change my PJs and find a dry place to sleep without waking my husband.</li>
+            <li>Raging arguments with family members that I couldn’t stop even though a voice inside me was telling me I was being irrational made me question my sanity.</li>
+            <li>Unexplained weight gain and belly fat that seemed to happen overnight.</li>
+            <li>Rushing to find a bathroom or planning my trips to include frequent bathroom stops for fear of an accident.</li></ul>
             <p>Everything felt out of my control and there was nowhere to turn for advice. Friends and family members knew what I was talking about or had different symptoms, but nobody had a solution. The general thought was “suck it up, buttercup” it’ll be over someday. But I worried that some things – like the osteoporosis and weight gain would stay with me and potentially get worse. I also saw the threat of chronic diseases like diabetes and cardiovascular disease as a big concern.</p>
             <p>Not willing to feel this out of balance any longer, I studied, found resources and worked with a functional medicine doctor to address my unique situation. I have come to learn how my hormone levels transitioned as my ovaries began to stop working and how bioidentical hormone replacement might help me to feel myself again. I have become focused on improving my bone density with a combination of yoga, HIIT and weightlifting. I have also begun to improve my incontinence with a regular pelvic floor strengthening routine. Starting Hormone Replacement Therapy has also helped with many of my symptoms.</p>
             <p>I understand it’s easy to feel alone during this time of life. And I know the struggle of plodding through all the advice from so many well-meaning menopause experts not knowing what would work for me. I’ll help you cut through all the noise of menopause advice to feel strong, clear and empowered in choosing how you will move through perimenopause and menopause. Together we’ll create a plan that supports your body, your hormones and your lifestyle so you can thrive with confidence and energy to feel like yourself again.</p>
