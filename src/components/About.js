62575f2035c08804eeb7710a605d726f7c6452ae
cxplanet <cxplanet@hotmail.com>
2025-12-02 15:06:44 -0800

    Update About.js
    Updated information and formating

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,19 @@ import './About.css';
 function About() {
     return (
         <div className="about">
-            <h1>My Story</h1>
-            <h2>Finding Inspiration in Every Turn</h2>
-            <p>Through my forties and fifties, I considered myself to be a healthy, fit individual. I didn’t have any chronic conditions (though parents and grands did). I had no allergies, I exercised regularly and I slept well. With the exception of semi-annual PCP and OB/GYN visits, I rarely found myself in need of a doctor. In my early 50s I was diagnosed with Osteopenia and 5 years later with Osteoporosis. When I began peri-menopause, my GYN asked if I wanted HRT, but since my mom had been through a bout of post-menopause breast cancer, I opted out.
-            Through self study and working with a Functional Medicine doctor, I have come to learn the importance of bio-identical HRT. I have become focused on improving my bone density with a combination of yoga, HIIT and weight lifting. I have also improved my incontinence with a regular pelvic floor strengthening routine. I have learned the importance of keeping my hormones inn balance and have started HRT.</p>
-            <h1>My Mission - Supporting Your Strength, Energy and Confidence</h1>
-            <p>I help women cut through the noise of menopause advice to feel strong, clear and empowered in their health decisions.</p>
-            <p> Whether you're considering HRT, struggling with sleep or stress, or looking to build strength and stability for the years ahead, I'm here to guide you. Together we'll create a plan that supports your body, your hormones and your lifestyl  - so you can thrive with confidence, energy and balance</p>
+            <h1>My Story - I’ve Been There, Too</h1>
+            <p>Through my forties and early fifties I considered myself to be a relatively healthy, fit individual. I exercised, ran and practiced yoga regularEnter ly and I slept well. With the exception of semi-annual PCP and GYN visits, I rarely found myself in need of a doctor.</p>
+            <p>Enter Perimenopause!</p>
+            <p><ul>
+            •An osteopenia diagnosis, followed in short order by osteoporosis made me feel old and frail despite my active lifestyle.
+            •Hot flashes that felt like I was roasting from the inside out.
+            •Night sweats that left me fumbling in the dark to change my PJs and find a dry place to sleep without waking my husband.
+            •Raging arguments with family members that I couldn’t stop even though a voice inside me was telling me I was being irrational made me question my sanity.
+            •Unexplained weight gain and belly fat that seemed to happen overnight.
+            •Rushing to find a bathroom or planning my trips to include frequent bathroom stops for fear of an accident.</ul?
+            <p>Everything felt out of my control and there was nowhere to turn for advice. Friends and family members knew what I was talking about or had different symptoms, but nobody had a solution. The general thought was “suck it up, buttercup” it’ll be over someday. But I worried that some things – like the osteoporosis and weight gain would stay with me and potentially get worse. I also saw the threat of chronic diseases like diabetes and cardiovascular disease as a big concern.</p>
+            <p>Not willing to feel this out of balance any longer, I studied, found resources and worked with a functional medicine doctor to address my unique situation. I have come to learn how my hormone levels transitioned as my ovaries began to stop working and how bioidentical hormone replacement might help me to feel myself again. I have become focused on improving my bone density with a combination of yoga, HIIT and weightlifting. I have also begun to improve my incontinence with a regular pelvic floor strengthening routine. Starting Hormone Replacement Therapy has also helped with many of my symptoms.</p>
+            <p>I understand it’s easy to feel alone during this time of life. And I know the struggle of plodding through all the advice from so many well-meaning menopause experts not knowing what would work for me. I’ll help you cut through all the noise of menopause advice to feel strong, clear and empowered in choosing how you will move through perimenopause and menopause. Together we’ll create a plan that supports your body, your hormones and your lifestyle so you can thrive with confidence and energy to feel like yourself again.</p>
         </div>
     );
 }
